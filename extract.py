14176477dbaa6492ab35c2ee1041eb7e50e15b2f
Mathieu Leplatre <mathieu@mozilla.com>
2019-02-12 22:28:18 +0100

    Extract as formatted JS code

--- extract.py
+++ extract.py
@@ -7,6 +7,6 @@ with open("good-vibes.html") as f:
     content = f.read()
 
 sorted_dict = {u: u for u in re.findall(URL_REGEX, content)}
-urls = [{"id": u.rsplit("/")[-1]} for u in sorted_dict.keys()]
+urls = [u.rsplit("/")[-1] for u in sorted_dict.keys()]
 
-print(json.dumps(urls, indent=2))
+print("\n".join(["{ id: \"%s\" }," % u for u in urls]))
