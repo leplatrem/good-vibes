2371dafd4080adc879d3fd49db98f18562a021c1
Mathieu Leplatre <mathieu@mozilla.com>
2019-10-17 23:06:07 +0200

    Full rewrite

--- extract.py
+++ extract.py
@@ -1,27 +1,65 @@
+"""
+
+1. Open WhatsApp Web
+2. Use Browser DevTools
+3. Save HTML to file on disk
+4. Run
+5. `git commit`
+6. `git push origin gh-pages`
+
+```
+$ YOUTUBE_KEY=AIzaSy....0Yc python3 extract.py < good-vibes.html
+```
+
+* See https://console.developers.google.com/apis/api/youtube.googleapis.com/
+
+"""
+import json
 import os
 import re
-import json
+import sys
 
 import requests
 
 
-URL_REGEX = r'https?://youtu[^"<\n]+'
-
-with open("good-vibes.html") as f:
-    content = f.read()
-
-sorted_dict = {u[-11:]: {"id": u[-11:]} for u in re.findall(URL_REGEX, content)}
-
-for e in json.load(open("data.json", "r")):
-    sorted_dict[e["id"]] = e
-
+JSON_FILE = "data.json"
 KEY = os.getenv("YOUTUBE_KEY")
-for i, obj in sorted_dict.items():
-    if "title" in obj:
-        continue
-    url = f"https://www.googleapis.com/youtube/v3/videos?id={i}&part=snippet&key={KEY}"
-    r = requests.get(url).json()
-    title = r["items"][0]["snippet"]["title"]
-    obj["title"] = title
+YOUTUBE_URL = f"https://www.googleapis.com/youtube/v3/videos?part=snippet&key={KEY}&id="
+URL_REGEX = r'https?://youtu[^\?&"<\n]{11,}'
 
-print(json.dumps(list(sorted_dict.values()), indent=2))
+# Load previous videos
+by_id = {}
+with open(JSON_FILE, "r") as f:
+    for e in json.load(f):
+        by_id[e["id"]] = e
+
+# Read HTML from stdin
+content = sys.stdin.read()
+
+# Parse HTML like it's 1992.
+urls = set(re.findall(URL_REGEX, content))
+
+for url in urls:
+    vid = url[-11:]
+
+    if vid in by_id:
+        print(vid, "Skip duplicate")
+        continue
+
+    try:
+        r = requests.get(YOUTUBE_URL + vid).json()
+        title = r["items"][0]["snippet"]["title"]
+
+        by_id[vid] = {
+            "id": vid,
+            "title": title,
+        }
+        print(vid, title)
+
+    except:
+        print(vid, r, file=sys.stderr)
+        raise
+
+# Overwrite with new list
+with open(JSON_FILE, "w") as f:
+    json.dump(list(by_id.values()), f, indent=2)
